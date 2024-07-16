805f6eec305261fc27487e0c3efc0dd65bcad187
DenisPapushaJava <DenisPapushaJava@gmail.com>
2024-07-17 01:05:34 +0300

    commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,17 @@
+import { useState } from 'react';
+import { StyledFlex } from '../Flex/styledFlex.ts';
+
 function App() {
-  return <>MVC</>;
+  const [increment, setIncrement] = useState(0);
+
+  return (
+    <>
+      <StyledFlex justify={'center'}>
+        {increment}
+        <button onClick={() => setIncrement((prev) => prev + 1)}>+</button>
+      </StyledFlex>
+    </>
+  );
 }
 
 export default App;
