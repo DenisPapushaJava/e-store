17eda4eb38c3da342c0a9c1193cfd62ebcdd2fef
DenisPapushaJava <DenisPapushaJava@gmail.com>
2024-07-22 22:53:49 +0300

    Switch to yarn

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,5 @@
-import { StyledFlex } from '../Flex/styledFlex.ts';
-import { useState } from 'react';
-import DataFetcher from '../DataFetcher';
 function App() {
-  const [info, setInfo] = useState<{ message: string } | null>(null);
-  const [error, setError] = useState<{ message: string } | null>(null);
-  return (
-    <>
-      <StyledFlex justify={'center'}>
-        <h1>Data from Server:</h1>
-        <DataFetcher
-          url="http://localhost:3000/api"
-          onData={setInfo}
-          onError={setError}
-        />
-        <p>{info?.message}</p>
-        <p>{error?.message ? error.message : '...Loading...'}</p>
-      </StyledFlex>
-    </>
-  );
+  return <>1</>;
 }
 
 export default App;
